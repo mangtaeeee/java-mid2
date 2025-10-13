3f200f98b5215de805b0bcd48b4a93fb46dbb1b6
mangtaeeee <osp9658@naver.com>
2025-10-13 18:01:43 +0900

    해시코드 미구현 equals 미구현 또는 해시코드만 구현 시

---
+++ src/collection/set/member/MemberOnlyHash.java
@@ -0,0 +1,29 @@
+package collection.set.member;
+
+import java.util.Objects;
+
+//해시코드만 재정의 했을때
+public class MemberOnlyHash {
+
+    private String id;
+
+    public MemberOnlyHash(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "MemberOnlyHash{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
